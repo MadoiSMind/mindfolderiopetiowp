fa43bef7db2c2601c06814dc212ef6d0ac7958a1
MadoiSMind <zcadqewsx1337@gmail.com>
2022-10-02 13:20:01 +0300

    Update Summon.py

--- Finder/Summon.py
+++ Finder/Summon.py
@@ -11,43 +11,49 @@ os.chdir(sys._MEIPASS)
 design = 'Summon\\design.ui'
 font = 'Summon\\font\\clacon2.ttf'
 
-
 class Ui(QtWidgets.QMainWindow):
 	def __init__(self):
 		super(Ui, self).__init__() # Call the inherited classes __init__ method
 		uic.loadUi(design, self) # Load the .ui file
 		QFontDatabase.addApplicationFont(font)
 		self.show() # Show the GUI
-		self.pushButton.clicked.connect(self.on_click)
+		self.checkNet()
+
+	def t(self):
+		t = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
+		return t
+
+	def checkNet(self):
+		try:
+			response = requests.get("http://github.com/") #Проверяем сервак гитхаба
+			if response.status_code == 200: #Все норм
+				self.textBrowser.append(f"[{self.t()}] [Connected]") #Пишем коннектед
+				self.pushButton.clicked.connect(self.on_click) #Подключаем кнопку
+				return
+		except requests.ConnectionError: #Все плохо
+			self.textBrowser.append(f"[{self.t()}] [No connetion]") #Пишем но коннектион
+			self.lineEdit.setReadOnly(True) #Деактивируем строку ввода, кнопка не активна ибо не поключенна изначально
+		
+	def create_message(self, page, code):
+		if page.status_code == 200:
+			self.textBrowser.append(f"[{self.t()}] Code: {code} - founded\n ˅ Read ˅ \n{self.soup.get_text()}")
+		elif page.status_code == 400:
+			self.textBrowser.append(f"[{self.t()}] Code: {code} - invalid")
+		elif page.status_code == 404:
+			self.textBrowser.append(f"[{self.t()}] Code: {code} - not found")
+		self.lineEdit.setText("")
 
 	def on_click(self):
 		code = self.lineEdit.text()
 		if code == "cls":
 			self.lineEdit.setText("")#cls
 			self.textBrowser.clear()#cls
-		else:
-			url = "https://raw.githubusercontent.com/MadoiSMind/mindfolderiopetiowp/main/"
-			url2 = url + code + ".txt"
-			page = requests.get(url2)
-			soup = BeautifulSoup(page.text, "html.parser")
-			if page.status_code == 200:
-				ts = time.time()
-				timestamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-				self.textBrowser.append("[" + timestamp + "] " + "[Connected]")
-				ts = time.time()
-				timestamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-				self.textBrowser.append("[" + timestamp + "] Code: " + code + " - founded\n\n" + soup.get_text())
-				self.lineEdit.setText("")#cls
-			elif page.status_code == 400:
-				ts = time.time()
-				timestamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-				self.textBrowser.append("[" + timestamp + "] " + "Code: " + code + " - invalid")
-				self.lineEdit.setText("")#cls
-			elif page.status_code == 404:
-				ts = time.time()
-				timestamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-				self.textBrowser.append("[" + timestamp + "] " + "Code:" + code + " - not found")
-				self.lineEdit.setText("")#cls
+			return
+		url = "https://raw.githubusercontent.com/MadoiSMind/mindfolderiopetiowp/main/"
+		url2 = url + code + ".txt"
+		page = requests.get(url2)
+		self.soup = BeautifulSoup(page.text, "html.parser")
+		self.create_message(page, code)
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
